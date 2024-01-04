5cd38877c21f549074371ee846413b7b6131cb99
ericq <qian212@purdue.edu>
2024-01-04 00:22:44 -0600

    test

--- grid.js
+++ grid.js
@@ -131,7 +131,7 @@ function processDataAndPlot(filter) {
 
         }];
         }
-          var config = { displayModeBar: true, responsive: true};
+          var config = { displayModeBar: true, responsive: true, scrollZoom: true, dragmode: false};
           // Create the Plotly graph after processing CSV data
           Plotly.newPlot('plot', data, layout, config);
 
